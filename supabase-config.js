64ef0b2d5916828ea0183e5586fc4074e316ae9f
Ineherte <75628978+Ineherte@users.noreply.github.com>
2026-09-02 12:13:11 +0000

    Corregir clave de Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // Pega aquí los datos públicos de tu proyecto Supabase.
 window.SUPABASE_CONFIG = {
   url: 'https://lnctwcizdjaeomvkcbst.supabase.co',
-  anonKey: 'sb_publishable_2OSFUZoF67md0pNbfhKMFQ_Zlt608ameyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuY3R3Y2l6ZGphZW9tdmtjYnN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNDY2NTEsImV4cCI6MjEwMzkyMjY1MX0.ETo5cWXr0jdcNIEstO_uaI53B50vyN9Azge2sKeBI4c'
+  anonKey: 'sb_publishable_2OSFUZoF67md0pNbfhKMFQ_Zlt608am'
 };
